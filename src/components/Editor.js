f8b9c2b7bd3b2606b1360f94798d84f529585b73
Joe Blank <joe.blank@live.com>
2018-05-30 11:39:57 -0600

    add in dangerously set html section

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,7 +6,8 @@ export default class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorState: null
+      editorState: null,
+      htmlToDisplay: ''
     };
     this.converter = new Showdown.Converter({
       tables: true,
@@ -20,16 +21,24 @@ export default class Editor extends React.Component {
     this.setState({ editorState });
   }
   markdownPreview(markdown) {
-    return Promise.resolve(this.converter.makeHtml(markdown))
+    let html = this.converter.makeHtml(markdown)
+    this.setState({htmlToDisplay: html})
+    return Promise.resolve(html)
   }
 
   render() {
     return (
-      <ReactMde
+      <div>
+        <ReactMde
         onChange={this.handleChange}
         editorState={this.state.editorState}
         generateMarkdownPreview={this.markdownPreview}
+        layout='vertical'
       />
+      <hr />
+      <h5>Dangerously Set HTML:</h5>
+      <div dangerouslySetInnerHTML={{__html: this.state.htmlToDisplay}}></div> 
+      </div> 
     );
   }
 }
